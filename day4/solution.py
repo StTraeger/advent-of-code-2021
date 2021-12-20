8f901ce27a4e9b28cec88a84d037e2648ee4a593
Stefan Träger <Stefan.Traeger@stud.fh-rosenheim.de>
2021-12-20 20:16:02 +0100

    Day 4, challenge 1: solved.

--- day4/solution.py
+++ day4/solution.py
@@ -1,4 +1,6 @@
-# Challenge 1
+from itertools import chain
+
+
 class BingoBoard:
     def __init__(self):
         self.position = {}
@@ -18,30 +20,29 @@ class BingoBoard:
         index = 0
         for i in range(5):
             self.board[i][row] = numbers[i]
+            self.position[numbers[i]] = (i, row)
             index += 1
             if index == 24:
                 break
 
-    def fillBingo(self, x, y):
+    def fill_bingo(self, x, y):
         self.bingo["row"][x] += 1
         self.bingo["col"][y] += 1
 
-    def isBingo(self):
+    def is_bingo(self):
         return 5 in self.bingo["row"] or 5 in self.bingo["col"]
 
     def update(self, val):
-        x, y = self.position[val]
-        self.board[x][y] = 'X'
-        self.fillBingo(x, y)
-
-    def __str__(self):
-        return board
+        x, y = self.position.get(val, (-1, -1))
+        if x != -1 and y != -1:
+            self.fill_bingo(x, y)
 
 
+# Challenge 1
 with open("input.txt") as file:
     content = file.read().splitlines()
 
-drawings = content[0]
+drawings = [int(d) for d in content[0].split(",")]
 boards = []
 
 board = BingoBoard()
@@ -54,7 +55,27 @@ for i in range(1, len(content)):
     if content[i] == '':
         i += 1
     else:
-        board.initialize(row, [int(l) for l in content[i].split()])
+        board.initialize(row, [int(value) for value in content[i].split()])
         row += 1
 
-print(drawings)
+isBingo = False
+winnerIndex = None
+winningDraw = drawIndex = 0
+
+for dIdx, drawing in enumerate(drawings):
+    for idx, board in enumerate(boards):
+        board.update(int(drawing))
+        if board.is_bingo():
+            winnerIndex = idx
+            winningDraw = drawing
+            isBingo = True
+            break
+    if isBingo is True:
+        drawIndex = dIdx
+        break
+
+result = sum([z for z in list(chain.from_iterable(boards[winnerIndex].board)) if z not in drawings[:drawIndex + 1]]) \
+         * winningDraw
+print("Solution: " + str(result))
+
+# Challenge 2
